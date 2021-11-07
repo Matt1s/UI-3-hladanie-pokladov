777dd9c875967e92536c8a27b62635b68f430dec
Matej Pakán <mattpakan@gmail.com>
2021-11-07 16:14:04 +0100

    Elitarizmus, vyber rodicov, krizenie, mutovanie hotove

--- hladanie_pokladov.py
+++ hladanie_pokladov.py
@@ -1,4 +1,5 @@
 from random import randint
+import time
 
 # Declaring variables
 grid_x = 0
@@ -10,6 +11,15 @@ treasures = []
 # Number of entities in single generation
 N_IN_GENERATION = 100
 MAX_INSTRUCTIONS = 500
+
+# Number of max possible generations
+MAX_GENERATIONS = 1000
+
+# Mutation rate in percent
+MUTATION_RATE = 1
+
+N_ELITES = 10
+N_PARENTS = 50
 
 generation = []
 
@@ -61,9 +71,6 @@ print("Štart:",start_x, start_y)
 print("Počet pokladov:", len(treasures))
 print("Poklady:",treasures)
 
-class Generation():
-    entities = []
-
 class Entity():
     def __init__(self):
         self.genome = []
@@ -83,11 +90,11 @@ class Entity():
         self.prints.append(direction)
 
 
-def VM(index, entity):
+def VM(entity):
     i = 0
     for i in range(len(entity.genome)):
 
-        if(MAX_INSTRUCTIONS > 500):
+        if(i > MAX_INSTRUCTIONS):
             print("Too many instructions!")
             return
         #print("Jedinec:",index,"gén:",i,"inštrukcia",gene[0:2])
@@ -95,30 +102,33 @@ def VM(index, entity):
         instruction = entity.genome[i][:2]
         if(instruction == '00'):
             # Increment
-            print("INCREMENT")
+            #print("INCREMENT")
             improved_gene = bin(int(entity.genome[i],2) + 1)[2:].zfill(8)
+            entity.genome[i] = improved_gene
         if(instruction == '00'):
             # Decrement
-            print(entity.genome[i])
-            print("DECREMENT")
+            #print(entity.genome[i])
+            #print("DECREMENT")
             improved_gene = bin(int(entity.genome[i],2) - 1)[2:].zfill(8)
-            print(improved_gene)
+            entity.genome[i] = improved_gene
+            #print(improved_gene)
         if(instruction == '01'):
-            print("JUMP")
+            #print("JUMP")
             # Jump on next gene
-            print(entity.genome[i])
+            #print(entity.genome[i])
             # Get last 6 characters from variable gene
             jump_gene = int(entity.genome[i][-6:])
             i = jump_gene
-            print(jump_gene)
+            #print(jump_gene)
             break
         if(instruction == '11'): # Instruction PRINT
             #print(i, entity.genome[i])
-            print("PRINT")
+            #print("PRINT")
             direction = entity.genome[i][-2:]
             entity.addDirection(direction)
-        i+=1
     #print(entity.prints)
+
+        i+=1
 
 found_treasures = []
 
@@ -129,7 +139,7 @@ def checkTreasure(entity):
             for found_treasure in found_treasures:
                 if(found_treasure == treasure):
                     return False
-            print("Treasure found!", treasure)
+            #print("Treasure found!", treasure)
             found_treasures.append([treasure[0], treasure[1]])
             entity.fitness += 1
             return True
@@ -149,23 +159,26 @@ def movePlayer(direction):
         else:
             start_y += 1
 
-    elif(direction == "P"):
+    elif(direction == "L"):
         if(start_x == 0):
             return False
         else:
             start_x -= 1
 
-    elif(direction == "L"):
+    elif(direction == "P"):
         if(start_x == grid_x-1):
             return False
         else:
             start_x += 1
     else:
         print("Error")
+    #print(direction)
+    #print(start_x,start_y)
     
 def rateEntity(entity):
     global start_x, start_y, start_pos
     moves = entity.prints
+    possible_moves = ["H","D","P","L"]
     for move in moves:
         if(move == "H"):
             movePlayer("H")
@@ -207,8 +220,132 @@ for count in range(N_IN_GENERATION):
 
 print("Počet jedincov v generácii: ",len(generation))
 index = 0
-for entity in generation:
-    VM(index, entity)
-    index += 1
-    rateEntity(entity)
-    print("Jedinec:",i,"Fitness:", entity.fitness)
+
+def pickElites(old_generation):
+
+    # Sorting generation by fitness
+    old_generation.sort(key=lambda x: x.fitness, reverse=True)
+
+    # Selecting best entities
+    elites = []
+    for i in range(N_ELITES):
+        # Reset prints and fitness
+        old_generation[i].fitness = 0
+        old_generation[i].prints = []
+        elites.append(old_generation[i])
+
+    return elites
+
+def pickParents(old_generation):
+    # Sorting generation by fitness
+    old_generation.sort(key=lambda x: x.fitness, reverse=True)
+
+    # Selecting best entities
+    parents = []
+    for i in range(N_PARENTS):
+        parents.append(old_generation[i])
+
+    return parents
+
+def crossGeneration(old_generation):
+    parents = pickParents(old_generation)
+
+    new_generation = []
+
+    for i in range(N_IN_GENERATION - N_ELITES):
+        parent1 = parents[randint(0,N_PARENTS-1)]
+        parent2 = parents[randint(0,N_PARENTS-1)]
+        child = Entity()
+
+        #print("PRINTS: ",child.prints)
+        for i in range(len(parent1.genome)):
+            if(randint(0,1) == 1):
+                child.genome.append(parent1.genome[i])
+            else:
+                child.genome.append(parent2.genome[i])
+
+            #mutate child gene with propability MUTATION_RATE in percent
+            if(randint(0,100) < MUTATION_RATE):
+                #print("Mutated!")
+                child.genome[i] = bin(randint(0,255))[2:].zfill(8)
+
+        #print("Crossing: ",parent1.fitness,"+",parent2.fitness)
+        new_generation.append(child)
+
+    return new_generation
+
+
+gen_number = 0
+curr_best_fitness = 0
+new_gen_best_fitness = 0
+best_entity = Entity()
+
+all_time_stopped = 0
+
+# start timer
+start = time.time()
+
+def newGeneration():
+    global generation, gen_number, curr_best_fitness, new_gen_best_fitness, best_entity
+
+    gen_number += 1
+
+    if(gen_number != 1):
+        new_generation = pickElites(generation)
+        new_children = (crossGeneration(generation))
+
+        generation = new_generation
+
+        generation.extend(new_children)
+        #print("Počet jedincov v generácii: ",len(generation))
+
+
+    best_entity = Entity()
+    best_entity.genome = []
+    best_entity.fitness = 0
+    best_entity.prints = []
+
+    for entity in generation:
+        VM(entity)
+        rateEntity(entity)
+        #print("Jedinec:",i,"Fitness:", entity.fitness)
+        if(entity.fitness > best_entity.fitness):
+            best_entity = entity
+            if(best_entity.fitness == len(treasures)):
+                print("WINNER")
+                print(best_entity.prints)
+                return True
+
+for i in range(MAX_GENERATIONS):
+
+    if(best_entity.fitness > curr_best_fitness):
+            curr_best_fitness = best_entity.fitness
+
+            print(str(gen_number) + ". generácia")
+            print("Best entity fitness:", best_entity.fitness)
+            print("Best entity moves:",best_entity.prints)
+            print("-------------------------------------")
+
+            print("Prajete si vytvoriť ďalšiu generáciu? [A/n]")
+            time_stop_start = time.time()
+            answer = input()
+            if(answer == "A" or answer == "a" or answer == ""):
+                time_stop_end = time.time()
+                time_stop = time_stop_end - time_stop_start
+                all_time_stopped += time_stop
+                newGeneration()
+
+            else:
+                time_stop_end = time.time()
+                time_stop = time_stop_end - time_stop_start
+                all_time_stopped += time_stop
+                print("Ukončené používateľom")
+                exit(0)
+    else:
+        if(newGeneration()):
+            break
+
+end_time = time.time()
+
+#print time elapsed and round it to two decimal places
+print("Prejdený čas:", round(end_time - start - all_time_stopped, 2), "sekúnd")
